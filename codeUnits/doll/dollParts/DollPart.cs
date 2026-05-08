7ab706bb2ecd9ecf24ca91ac2b88b4cd724e5e1c
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-08 12:11:11 +0300

    Create DollPart.cs

---
+++ codeUnits/doll/dollParts/DollPart.cs
@@ -0,0 +1,33 @@
+using NTC.MonoCache;
+using System;
+using UnityEngine;
+
+namespace GentianoseRealDolls
+{
+    public class DollPart : MonoCache
+    {
+        // TODO: сделать поля открытыми для UI
+        // звери больше не зависят от UI, требования принципа DI
+        // Каждые 0.1 с обновляется HUD
+        
+        protected DollController m_DollContr;
+        protected Party m_Party;
+        protected Animator m_Animator;
+
+
+
+        public void Construct(DollController contr, Party party)
+        {
+            m_DollContr = contr;
+            m_Party = party;
+        }
+
+        
+        public virtual void SetAimInput(Vector2 aimInput) { }
+        
+
+        public virtual void Use(Vector2 aimInput) { }
+    }
+}
+
+
