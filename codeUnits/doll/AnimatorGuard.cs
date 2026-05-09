bb4ed4e8f1d9843dcb8f807a8c373e3ae3a328ad
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-09 21:43:23 +0300

    Update AnimatorGuard.cs

--- codeUnits/doll/AnimatorGuard.cs
+++ codeUnits/doll/AnimatorGuard.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 
 public class AnimatorGuard : MonoBehaviour, IAnimatorController
 {
+    [Header("Tags: * idle \n+ walk " +
+        "\n++ trot/amble/canter \n+++ gallop " +
+        "\n. normal attack \n- charged attack " +
+        "\n! lesser skill \n& spray")]
+
     [SerializeField] private Animator m_Animator;
-    [SerializeField] private int m_AnimID;
+    [SerializeField] private int m_AnimID;   
+    
 
     public void SetAnimation(int anim)
     {
-        m_Animator.SetInteger("Autom", anim);
         m_AnimID = anim;
+        m_Animator.SetInteger("Autom", m_AnimID);
+        print($"Inflicted animation #{m_AnimID}");
     }
 
     public bool IsIdle()
     {
-        if (m_Animator.GetCurrentAnimatorStateInfo(0).IsTag("-"))
+        if (m_Animator.GetCurrentAnimatorStateInfo(0).IsTag("*"))
             return true;
         return false;
     }
