70e03aaf85725314bbbc6c8245856ebc56ff6faf
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-09 21:57:26 +0300

    Update ShipInputController.cs

--- codeUnits/Player/ShipInputController.cs
+++ codeUnits/Player/ShipInputController.cs
@@ -1,17 +1,36 @@
-using GentianoseRealDolls;
 using NTC.MonoCache;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace SpaceShooter
+namespace GentianoseRealDolls
 {
-    public class ShipInputController : MonoCache
+    public class MoveInputController : MonoCache
     {
+        [Header("***")]
         [SerializeField] private ControlModeData controlModeData;
-       
+
+        [SerializeField] private VirtualGamePad m_VirtualGamePad;
+        [SerializeField] private CombatDashboard m_CombatDashboard;
+        [SerializeField] private Party m_Party;
+
+        [Header("***")]
+
+        [SerializeField] private SpaceShip m_TargetShip;
+        [SerializeField] private DollController m_TargetDoll;
+        [SerializeField] private Animator m_Animator;
+
+        //lemur
+        public static bool mouseTorque = true;
+
+        Vector2 moveInput;
         public void SetTargetShip(SpaceShip ship) => m_TargetShip = ship;
 
-       
+
+        public void SetTargetDoll(DollController doll)
+        {
+            m_TargetDoll = doll;
+            m_TargetShip = doll.PetAsSpaceShip;
+        }
 
         public void Construct(VirtualGamePad virtualGamePad)
         {
@@ -20,14 +39,9 @@ namespace SpaceShooter
 
         //private bool m_Landed;
 
-        [SerializeField] private SpaceShip m_TargetShip;
-
-        [SerializeField] private VirtualGamePad m_VirtualGamePad;
-        [SerializeField] private CombatDashboard m_CombatDashboard;
-
         private void Start()
         {
-
+            m_TargetShip = m_Party.ActiveDoll.PetAsSpaceShip;
 
             if (controlModeData.Control == ControlModeData.ControlMode.Keyboard)
             {
@@ -61,7 +75,6 @@ namespace SpaceShooter
 
 
             if (controlModeData.Control == ControlModeData.ControlMode.Keyboard)
-                // ControlKeyboard();
                 ControlNew();
 
             if (controlModeData.Control == ControlModeData.ControlMode.Mobile)
@@ -74,35 +87,38 @@ namespace SpaceShooter
         }
         private void ControlNew()
         {
-
             float thrust = 0;
             float torque = 0;
 
-            thrust = moveInput.y;
-            torque = -moveInput.x;
-
+            if (moveInput != Vector2.zero)
+            {
+                thrust = moveInput.y;
+                torque = -moveInput.x;
+            }
 
             m_TargetShip.ThrustControl = thrust;
             m_TargetShip.TorqueControl = torque;
+
+            
+            m_TargetDoll?.UpdateMoveInput(moveInput);
         }
         
         private void ControlMobile()
         {
             Vector3 dir = m_VirtualGamePad.VirtualJoystick.Value;
 
-            m_TargetShip.ThrustControl = dir.y;
-            m_TargetShip.TorqueControl = -dir.x;
+            if (dir != Vector3.zero)
+            {
+                m_TargetShip.ThrustControl = dir.y;
+                m_TargetShip.TorqueControl = -dir.x;
+            }   
         }
-        [SerializeField] private Animator m_Animator;
-        //lemur
-        public static bool mouseTorque = true;
 
         public void Leap()
         {
             m_TargetShip.Leap();
         }
 
-        Vector2 moveInput;
         // Этот метод вызывается Player Input,
         // когда срабатывает действие Move
         public void OnMove(InputAction.CallbackContext context)
@@ -110,8 +126,11 @@ namespace SpaceShooter
             // Считываем значение Vector2 из Input System
             // Оно описывает направление движения
             moveInput = context.ReadValue<Vector2>();
-        } 
-        
+
+            
+        }  
+      
+
         // Этот метод вызывается при нажатии кнопки Jump
         public void OnJump(InputAction.CallbackContext context)
         {
@@ -119,76 +138,18 @@ namespace SpaceShooter
             // а не отменено или в процессе
             if (!context.performed) return;
 
-            // В качестве наглядного эффекта
-            // просто поворачиваем объект на 90 градусов
             Leap();
         }
 
-        private void ControlKeyboard()
-        {
-
-            float thrust = 0;
-            float torque = 0;
-
-            float jump = 0;
-
-            if (Input.GetKey(KeyCode.W))
-                thrust = 1.0f;
-
-            if (Input.GetKey(KeyCode.S))
-                thrust = -1.0f;
-
-            if (Input.GetKey(KeyCode.A))
-                torque = 1.0f;
-
-            if (Input.GetKey(KeyCode.D))
-                torque = -1.0f;
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-
-                // m_TargetShip.Fire(TurretMode.Primary);
-                jump = 1.0f;
-                m_TargetShip.Leap();
-            }
-
-            if (Input.GetKey(KeyCode.X))
-            {
-                m_TargetShip.Fire(TurretMode.Secondary);
-            }
-
-          
-
-            if (mouseTorque)
-            {
-                if (Input.GetAxis("Mouse X") > 0 && Input.GetAxis("Mouse X") < 0.6f)
-                {
-                    torque = -1.0f;
-                }
-                if (Input.GetAxis("Mouse X") < 0 && Input.GetAxis("Mouse X") > -0.6f)
-                {
-                    torque = 1.0f;
-                }
-
-             
-            }
-         
-            if (Input.GetKeyDown(KeyCode.LeftAlt))
-            {
-                mouseTorque = !mouseTorque;
-            }
-            m_TargetShip.ThrustControl = thrust;
-            m_TargetShip.TorqueControl = torque;
-            m_TargetShip.JumpControl = jump;    
-        }
+       
 
         private void ControlKeyboardAndMobile()
-        {            
-                ControlKeyboard();          
-                ControlMobile();
+        {
+            ControlNew();
+            ControlMobile();
         }
 
-     
+        
     }
 }
 
