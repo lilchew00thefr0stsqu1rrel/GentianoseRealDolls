c9d4f0264969d0721e9baa7517265029321df7f2
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-10 17:25:31 +0300

    Update TimePastStats.cs

--- codeUnits/Player/Beasts_aka_Dolls/TimePastStats.cs
+++ codeUnits/Player/Beasts_aka_Dolls/TimePastStats.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using TowerDefense;
 using UnityEngine;
 
@@ -6,49 +7,33 @@ namespace GentianoseRealDolls
 {
     public class TimePastStats : MonoBehaviour
     {
-        private const string fileName1 = "timeCurr.dat";
-        private const string fileName2 = "timePrev.dat";
+        private const string fileName = "timePrev.dat";
 
-        private long m_CurrentTime;
-        private long m_PreviousTime;
+        [SerializeField] private long m_CurrentTime;
+        [SerializeField] private long m_PreviousTime;
 
-        private long m_TimeDifference;
+        [SerializeField] private long m_TimeDifference;
 
         public long ReadTime()
         {
-            //Saver<long>.TryLoad(fileName1, ref m_CurrentTime);
-            Saver<long>.TryLoad(fileName2, ref m_PreviousTime);
+            Saver<long>.TryLoad(fileName, ref m_PreviousTime);
 
             m_CurrentTime = DateTime.Now.Ticks / 600000000;
             m_TimeDifference = m_CurrentTime - m_PreviousTime;
 
             return m_TimeDifference;
         }
-        public void RefreshTime()
-        {
-        }
-
-        public void WriteTimeDestroy()
-        {
-            Saver<long>.Save(fileName2, m_PreviousTime);
-        }
 
         private void OnDestroy()
         {
-            ////m_PreviousTime = m_CurrentTime;
-            //
-            //// Saver<long>.Save(fileName2, m_PreviousTime);
+            WriteTimeFinal();
         }
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+
+        public void WriteTimeFinal()
         {
-
+            Saver<long>.Save(fileName, DateTime.Now.Ticks / 600000000);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 }
+
