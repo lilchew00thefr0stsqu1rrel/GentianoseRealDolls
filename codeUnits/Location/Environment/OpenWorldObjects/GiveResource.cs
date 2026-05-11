146407fa5844e0399a98d3e3eda9b0ecb20fb0a7
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-11 19:24:39 +0300

    Update GiveResource.cs

--- codeUnits/Location/Environment/OpenWorldObjects/GiveResource.cs
+++ codeUnits/Location/Environment/OpenWorldObjects/GiveResource.cs
@@ -7,49 +7,43 @@ using VContainer.Unity;
 
 namespace GentianoseRealDolls
 {
-    public class GiveResource : MonoBehaviour
+    public class GiveResource : InteractableObject
     {
-        protected Dashboard m_Dashboard;
-
-        [Inject]
-        public void Construct(Dashboard dashboard)
-        {
-            m_Dashboard = dashboard;
-        }
+        public static event Action<int, string, GiveResource> OnWentToResource;
 
         [SerializeField] private InventoryItem m_Item;
         [SerializeField] private int m_YieldAmount;
 
         [SerializeField] private List<GameObject> m_ItemsInWorld;
-        private IObjectResolver m_Resolver;
-        private void Awake()
-        {
-        }
+
+        
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             print("00");
-            __ = false;
+            __ = false; 
+            tipID = 6;
         }
 
         private DateTime useDateTime;
-        // Update is called once per frame
-        void Update()
-        {
-          
-        }
+
         private bool __;
-        int tipID = 6;
+        
 
         private void OnTriggerEnter(Collider other)
         {
             if (m_Item != null && !__ && m_ItemsInWorld.Count > 0)
             {
-                if (other.transform.root.GetComponent<Doll>() != null)
+                var p = other.GetComponent<Party>();
+                if (p != null)
                 {
                     print("Boing");
-                    m_Dashboard.ShowInteractTip(tipID, m_Item.itemName, this);
+
+
+                    // m_Dashboard.ShowInteractTip(tipID, m_Item.itemName, this);
+                    OnWentToResource(tipID, m_Item.itemName, this);
+
                 }
             }
 
