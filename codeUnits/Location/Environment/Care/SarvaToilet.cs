3e13438cba0436d4c669235d5ecf6d1dd94dbb9c
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-11 22:16:04 +0300

    Update SarvaToilet.cs

--- codeUnits/Location/Environment/Care/SarvaToilet.cs
+++ codeUnits/Location/Environment/Care/SarvaToilet.cs
@@ -2,34 +2,28 @@ using UnityEngine;
 
 namespace GentianoseRealDolls
 {
-    public class SarvaToilet : MonoBehaviour
+    public class SarvaToilet : InteractableObject
     {
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-
+            tipID = 10;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         public static bool CanPoop;
 
         [SerializeField] private Collider m_Collider;
-        private void OnTriggerEnter(Collider other)
+
+        protected override void OnDollCome(Party p)
         {
-            var dc = other.transform.root.GetComponent<DollController>();
-            if (dc != null && dc.ActiveDollInPartyStatus)
+            if (p != null)
                 CanPoop = true;
         }
 
-        private void OnTriggerExit(Collider other)
+        protected override void OnDollGone(Party p)
         {
-            var dc = other.transform.root.GetComponent<DollController>();
-            if (dc != null && dc.ActiveDollInPartyStatus)
+            if (p != null)
                 CanPoop = false;
         }
     }
