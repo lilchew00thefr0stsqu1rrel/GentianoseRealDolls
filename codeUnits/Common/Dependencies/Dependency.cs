e18693f7fa3a61f5302b08ecef3be4bfacfc7b52
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-13 16:11:43 +0300

    Add files via upload

---
+++ codeUnits/Common/Dependencies/Dependency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Dependency : MonoBehaviour
+{
+    protected virtual void BindAll(MonoBehaviour mono) {    }
+    protected void FindAllObjectToBind()
+    {
+
+        MonoBehaviour[] monoInScene = FindObjectsByType<MonoBehaviour>(sortMode: FindObjectsSortMode.InstanceID);
+
+        for (int i = 0; i < monoInScene.Length; i++)
+        {
+            BindAll(monoInScene[i]);
+
+        }
+    }
+    protected virtual void Bind<T>(MonoBehaviour bindObject, MonoBehaviour target) where T : class
+    {
+        if (target is IDependency<T>) (target as IDependency<T>).Construct(bindObject as T);
+    }
+
+}
---
+++ codeUnits/Common/Dependencies/SceneDependenciesContainer.cs
@@ -0,0 +1,28 @@
+﻿using SpaceShooter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GentianoseRealDolls
+{
+
+    public class SceneDependenciesContainer : Dependency
+    {
+        [SerializeField] private Level level;
+
+
+        protected override void BindAll(MonoBehaviour monoBehaviourInScene)
+        {
+            Bind<Level>(level, monoBehaviourInScene);
+        } 
+
+        //private void Bind(MonoBehaviour mono)
+    
+    
+
+        private void Awake()
+        {
+            FindAllObjectToBind();
+        }
+    }
+}
