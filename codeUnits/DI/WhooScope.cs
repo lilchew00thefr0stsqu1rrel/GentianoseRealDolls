413d101db563f6827a1a35010dfeb6691c311763
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-13 16:13:58 +0300

    Update WhooScope.cs

--- codeUnits/DI/WhooScope.cs
+++ codeUnits/DI/WhooScope.cs
@@ -8,7 +8,15 @@ public class WhooScope : LifetimeScope
     protected override void Configure(IContainerBuilder builder)
     {
         Debug.Log("WhooScope Started");
+
+        builder.RegisterComponentInHierarchy<InteractableObject>();
+        builder.RegisterComponentInHierarchy<SarvaToilet>();
+        builder.RegisterComponentInHierarchy<Mechanism>();
+        builder.RegisterComponentInHierarchy<Shop>();
+
         builder.RegisterComponentInHierarchy<GiveResource>();
         builder.RegisterComponentInHierarchy<ZifferGate>();
+        builder.Register<ThrowBeastsToScene>(Lifetime.Scoped);
+        builder.Register<EnemyWildlifeSpawner>(Lifetime.Scoped);
     }
 }
