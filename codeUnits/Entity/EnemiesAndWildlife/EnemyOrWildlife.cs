36b5dd0f5994ad9c7612a63d336914666c82fc02
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-17 14:07:11 +0300

    Update EnemyOrWildlife.cs

--- codeUnits/Entity/EnemiesAndWildlife/EnemyOrWildlife.cs
+++ codeUnits/Entity/EnemiesAndWildlife/EnemyOrWildlife.cs
@@ -1,4 +1,7 @@
+using NTC.Pool;
 using System;
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace GentianoseRealDolls
@@ -14,13 +17,35 @@ namespace GentianoseRealDolls
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-
+            Despawn();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        private IEnumerator Despawn()
+        {
+            yield return new WaitForSeconds(60);
+
+            var colls = Physics.OverlapSphere(transform.position, 100);
+
+            bool partyHere = false;
+
+            for (int i = 0; i < colls.Length; i++)
+            {
+                if (colls[i].GetComponent<Party>())
+                {
+                    partyHere = true;
+                }
+            }
+
+            if (!partyHere)
+            {
+                NightPool.Despawn(gameObject);
+            }
         }
     }
 }
