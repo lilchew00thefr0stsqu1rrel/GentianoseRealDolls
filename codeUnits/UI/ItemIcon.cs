c5007c1bf894af41ec62d89654109293b472c4f2
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-26 10:42:40 +0300

    Update ItemIcon.cs

--- codeUnits/UI/ItemIcon.cs
+++ codeUnits/UI/ItemIcon.cs
@@ -1,10 +1,17 @@
 using GentianoseRealDolls;
 using UnityEngine;
 using UnityEngine.UI;
+using VContainer;
 
 public class ItemIcon : MonoBehaviour
 {
-    
+
+    [Inject]
+    public void Construct(Inventory obj)
+    {
+        m_Inventory = obj;
+    }
+    [SerializeField] private Inventory m_Inventory;
 
     [SerializeField] private InventoryItem inventoryItem;
     public InventoryItem InventoryItem => inventoryItem;
@@ -12,13 +19,49 @@ public class ItemIcon : MonoBehaviour
 
     public void Initialise()
     {
-        var amount = Inventory.Instance.GetItemAmount(inventoryItem.itemID);
+        var amount = m_Inventory.GetItemAmount(inventoryItem.itemID);
         gameObject.SetActive(amount > 0);
        
-        amountText.text = amount.ToString();    
-
+        amountText.text = amount.ToString();
+        
+        print("Amount " + amount + " ITEmID "+ inventoryItem.itemID);
        // print("Init " + inventoryItem.itemID + "  " + amount);
 
         //Inventory.SaveInventory(inventoryItem, amount);
+    }
+
+    [SerializeField] private Image image;
+
+
+   [SerializeField]  private int m_Amount;
+    public void InitialiseSetItem(InventoryItem item)
+    {
+        inventoryItem = item;   
+        image.sprite = item.icon;
+
+        var amount = m_Inventory.GetItemAmount(inventoryItem.itemID);
+      
+        m_Amount = amount;
+        amountText.text = amount.ToString();
+
+        // print("Init " + inventoryItem.itemID + "  " + amount);
+
+        //Inventory.SaveInventory(inventoryItem, amount);
+    }
+    public void InitialiseSetImage()
+    {
+        image.sprite = inventoryItem.icon;
+
+        var amount = m_Inventory.GetItemAmount(inventoryItem.itemID);
+        
+
+        amountText.text = amount.ToString();
+
+    }
+
+    public void SetNull()
+    {
+        image.sprite = null;
+        amountText.text = "---";
     }
 }
