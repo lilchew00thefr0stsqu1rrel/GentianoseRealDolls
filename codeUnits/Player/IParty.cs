d175a1f2689d0cf371b6174c0179b687262f45cb
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-26 10:51:49 +0300

    Add files via upload

---
+++ codeUnits/Player/IParty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GentianoseRealDolls
+{
+    public interface IParty
+    {
+        public void InitDolls(int mapID, AllDollCharacters adc, 
+            AllDollPositions adp, AllDollSleeps ads, long time);
+    }
+}
