bd0ab5655c3e786a71ba83834ef4959b166e3e0d
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-26 10:32:12 +0300

    Update GiveResource.cs

--- codeUnits/Location/Environment/OpenWorldObjects/GiveResource.cs
+++ codeUnits/Location/Environment/OpenWorldObjects/GiveResource.cs
@@ -12,12 +12,21 @@ namespace GentianoseRealDolls
         public static event Action<int, string, GiveResource> OnWentToResource;
         public static event Action OnLeaveResource;
 
+        public static event Action<InventoryItem, int> OnTake;
+
         [SerializeField] private InventoryItem m_Item;
         [SerializeField] private int m_YieldAmount;
 
         [SerializeField] private List<GameObject> m_ItemsInWorld;
 
-        
+        [Inject]
+        public void Construct(Inventory obj)
+        {
+            m_Inventory = obj;
+        }
+        [SerializeField] private Inventory m_Inventory;
+
+
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
@@ -52,7 +61,8 @@ namespace GentianoseRealDolls
         {
             if (m_ItemsInWorld.Count > 0)
             {
-                Inventory.Instance.AddItemInstances(m_Item, 1);
+                OnTake(m_Item, 1);
+                //m_Inventory.AddItemInstances(m_Item, 1);
                 print("do");
                 InventoryController.Instance.InitAllItems();
                 NightPool.Despawn(m_ItemsInWorld[0]);
