67ed856307dff1e3bfde9f6122d27ac821bcf3b4
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-05-30 06:35:47 +0300

    Update InteractableObject.cs

--- codeUnits/Location/Environment/InteractableObject.cs
+++ codeUnits/Location/Environment/InteractableObject.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 using VContainer;
 
 namespace GentianoseRealDolls
 {
+    [RequireComponent(typeof(BoxCollider))]
     // Original name: Stove
     public class InteractableObject : MonoBehaviour
     {
+        [SerializeField] private BoxCollider m_Collider;
 
         public static event Action<int> OnCameToInteract;
 
@@ -17,27 +21,54 @@ namespace GentianoseRealDolls
         {
             m_Dashboard = obj;
         }
+        private void Awake()
+        {
+            m_Collider = GetComponent<BoxCollider>();
+        }
+
 
         [Tooltip("0 - stove; 1 - table; 6 - resource; 7 - shop")]
         [SerializeField] protected int tipID = 0;
+        [SerializeField] Party partyWisp;
+
+
 
 
         private void OnTriggerEnter(Collider other)
         {
-            var partyWisp = other.GetComponent<Party>();
+            partyWisp = other.GetComponent<Party>();
             if (partyWisp != null)
             {
                 print("Yoink" + gameObject.name + other.name);
                 OnDollCome(partyWisp);
             }
         }
+        float time = 0;
+
+        private void OnTriggerStay(Collider other)
+        {
+            time += Time.deltaTime;
+            if (time > 1)
+            {
+                partyWisp = other.GetComponent<Party>();
+                if (partyWisp != null)
+                {
+                    print("Yoink" + gameObject.name + other.name);
+                    OnDollCome(partyWisp);
+                }
+
+                time = 0;
+            }
+            
+        }
 
         private void OnTriggerExit(Collider other)
         {
-            var partyWisp = other.GetComponent<Party>();
+            partyWisp = other.GetComponent<Party>();
             if (partyWisp != null)
             {
-                OnDollGone(partyWisp);
+                OnDollGone();
+                partyWisp = null;
             }
         }
         
@@ -48,7 +79,7 @@ namespace GentianoseRealDolls
             if (m_Dashboard)
                 m_Dashboard.ShowInteractTip(tipID);
         }
-        protected virtual void OnDollGone(Party p)
+        protected virtual void OnDollGone()
         {
             if (m_Dashboard)
                 m_Dashboard.HideInteractTip();
