e250b40fd8b0e3bc164fae439e853af51f6240d6
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-06-02 16:49:39 +0300

    Update TimePastStats.cs

--- codeUnits/Player/TimePastStats.cs
+++ codeUnits/Player/TimePastStats.cs
@@ -7,21 +7,68 @@ namespace GentianoseRealDolls
 {
     public class TimePastStats : MonoBehaviour
     {
-        private const string fileName = "timePrev.dat";
+
 
         [SerializeField] private long m_CurrentTime;
         [SerializeField] private long m_PreviousTime;
 
         [SerializeField] private long m_TimeDifference;
+        [SerializeField] private long m_TimeDifference5min;
+        [SerializeField] private long m_TimeDifference15min;
+        [SerializeField] private UnityEngine.UI.Text m_DebugText;
 
-        public long ReadTime()
+        public enum TimeIntervals
         {
-            Saver<long>.TryLoad(fileName, ref m_PreviousTime);
+            PooTime,
+            BathTime,
+            FoodTime
+        }
 
-            m_CurrentTime = DateTime.Now.Ticks / 600000000;
-            m_TimeDifference = m_CurrentTime - m_PreviousTime;
+        public long ReadTime(TimeIntervals timeType = TimeIntervals.PooTime)
+        {
+            if (timeType == TimeIntervals.FoodTime)
+            {
+                Saver<long>.TryLoad(WhooSettings.fileNameTime, ref m_PreviousTime);
+                if (m_PreviousTime == 0)
+                {
+                    m_PreviousTime = DateTime.Now.Ticks / 9000000000;
+                }
 
-            return m_TimeDifference;
+                m_CurrentTime = DateTime.Now.Ticks / 9000000000;
+
+                m_TimeDifference = m_CurrentTime - m_PreviousTime;
+
+                return m_TimeDifference;
+            }
+
+            if (timeType == TimeIntervals.BathTime)
+            {
+                Saver<long>.TryLoad(WhooSettings.fileNameTime, ref m_PreviousTime);
+                if (m_PreviousTime == 0)
+                {
+                    m_PreviousTime = DateTime.Now.Ticks / 3000000000;
+                }
+
+                m_CurrentTime = DateTime.Now.Ticks / 3000000000;
+
+                m_TimeDifference = m_CurrentTime - m_PreviousTime;
+
+                return m_TimeDifference;
+            }
+
+            Saver<long>.TryLoad(WhooSettings.fileNameTime, ref m_PreviousTime);
+                if (m_PreviousTime == 0)
+                {
+                    m_PreviousTime = DateTime.Now.Ticks / 600000000;
+                }
+
+                m_CurrentTime = DateTime.Now.Ticks / 600000000;
+
+                m_TimeDifference = m_CurrentTime - m_PreviousTime;
+
+               return m_TimeDifference;
+            
+            
         }
 
         private void OnDestroy()
@@ -29,11 +76,17 @@ namespace GentianoseRealDolls
             WriteTimeFinal();
         }
 
+        //16V26 14:42
+        private void OnApplicationPause(bool pause)
+        {  
+            if (pause) //В 0.4.1
+                WriteTimeFinal();
+        }
+
         public void WriteTimeFinal()
         {
-            Saver<long>.Save(fileName, DateTime.Now.Ticks / 600000000);
+            Saver<long>.Save(WhooSettings.fileNameTime, DateTime.Now.Ticks / 600000000);
         }
 
     }
 }
-
