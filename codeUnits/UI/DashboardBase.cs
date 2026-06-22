1e5044da6809fd017cae4dd0e7367fc29c9fab65
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-06-22 11:41:25 +0300

    Create DashboardBase.cs

---
+++ codeUnits/UI/DashboardBase.cs
@@ -0,0 +1,18 @@
+using NTC.MonoCache;
+using UnityEngine;
+
+namespace GentianoseRealDolls
+{
+
+    public class DashboardBase : MonoCache
+    {
+        public virtual void UpdateUI()
+        {
+
+        }
+        public virtual void SetDoll(Doll doll)
+        {
+
+        }
+    }
+}
